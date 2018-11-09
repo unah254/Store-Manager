df7178e020daabc025f005ec6b794666ae51aede
unah254 <greisunah@gmail.com>
2018-11-09 08:35:10 +0300

    [feature #161810232] Created signup

---
+++ UI/static/js/signup.js
@@ -0,0 +1,38 @@
+// // Register a new User
+
+let signupuser = document.getElementById('signupbox')
+if (signupuser){
+    signupuser.addEventListener('submit', signup);
+    
+function signup(e){
+    e.preventDefault();
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+   
+    let signupUrl = 'http://127.0.0.1:5000/api/v2/signup';
+    let token = localStorage.getItem('token')
+
+    
+    fetch(signupUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type':'application/json',
+            "Authorization": "Bearer "+token
+        },
+        body:JSON.stringify({email:email,password:password,})
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message === `user with ${email} already exists`){
+                document.getElementById("mymessage").innerHTML = data.message
+                document.getElementById("mymessage").style.color = 'red';
+            }
+            if (data.message === `user ${email} created successfully`){
+                document.getElementById("mymessage").innerHTML = data.message
+                document.getElementById("mymessage").style.color = 'green';
+                window.location.assign("../admin/login.html");
+            }
+        })
+    }
+  }

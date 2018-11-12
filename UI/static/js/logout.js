b94868b7ade3e769dafa49393ec0f5121eba73d0
unah254 <greisunah@gmail.com>
2018-11-13 02:03:26 +0300

    [feature] finish logout

--- UI/static/js/logout.js
+++ UI/static/js/logout.js
@@ -1,4 +1,28 @@
-function logout(){
-	localStorage.clear()
+const logoutUrl = "https://store-management-app.herokuapp.com/api/v2/logout"
+
+let token = localStorage.getItem('token')
+if (!token){
+    window.location ='http://127.0.0.1:5500/UI/admin/login.html'
+}
+
+
+
+
+    function logout(){
+    
+fetch(logoutUrl, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-type':'application/json',
+        'Authorization':'Bearer ' +token
+    }})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    })
+    localStorage.removeItem('token')
+    window.location ='http://127.0.0.1:5500/UI/admin/login.html'
 
 }
+logout()

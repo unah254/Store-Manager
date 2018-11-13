137d446dc639ac47aa50695e4db8b2208505112e
unah254 <greisunah@gmail.com>
2018-11-13 11:26:48 +0300

    [feature] add record

--- UI/static/js/create_record.js
+++ UI/static/js/create_record.js
@@ -5,35 +5,40 @@ if (addRecord){
 }    
 function create(e){
     e.preventDefault();
-    let product_name = document.getElementById('product_name').value;
-    let price = document.getElementById('product_price').value;
-    let quantity = document.getElementById('quantity_price').value;
-    let category = document.getElementById('category').value;
+    let product_id = document.getElementById('product_id').value;
+    let price = document.getElementById('price').value;
+    let quantity = document.getElementById('quantity').value;
+    let creator = document.getElementById('category').value;
     let token = window.localStorage.getItem("token");
 
-    let productUrl = 'https://store-management-app.herokuapp.com/api/v2/product';
+    if (!token){
+        window.location ='http://127.0.0.1:5500/UI/admin/login.html'
+    }
+
+
+    let recordUrl = 'https://store-management-app.herokuapp.com/api/v2/product';
     
-    fetch(productUrl, {
+    fetch(recordUrl, {
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
             Authorization: `Bearer ${token}`,
             'Content-type':'application/json'
         },
-        body:JSON.stringify({name:product_name,price:price,quantity:quantity,category:category})
+        body:JSON.stringify({id:product_id,price:price,quantity_to_sell:quantity,creator_name:creator})
         })
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
            
     
-            if (data.message==="product already exists, please update product"){
+            if (data.message==="product does not exist"){
                 // if request is unsuccessful
                 document.getElementById('mymessage').style.color = 'red'
                 document.getElementById('mymessage').innerHTML = data.message
             }
             
-            if (data.message === "product successfuly created"){
+            if (data.message === "record successfuly created"){
                 // if request is successful
                 document.getElementById('mymessage').style.color = 'green'
                 document.getElementById('mymessage').innerHTML = data.message

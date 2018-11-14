eec7a051ce3bfe1e59e254f14fd32f1d3f6610ac
unah254 <greisunah@gmail.com>
2018-11-14 08:09:38 +0300

    [featire] add create record

--- UI/static/js/create_record.js
+++ UI/static/js/create_record.js
@@ -5,27 +5,27 @@ if (addRecord){
 }    
 function create(e){
     e.preventDefault();
-    let product_id = document.getElementById('product_id').value;
-    let price = document.getElementById('price').value;
-    let quantity = document.getElementById('quantity').value;
-    let creator = document.getElementById('category').value;
+    let product_id =parseInt(document.getElementById('product_id').value);
+    let price =parseInt(document.getElementById('price').value);
+    let quantity_to_sell =parseInt(document.getElementById('quantity').value);
     let token = window.localStorage.getItem("token");
+    
 
     if (!token){
         window.location ='http://127.0.0.1:5500/UI/admin/login.html'
     }
 
 
-    let recordUrl = 'https://store-management-app.herokuapp.com/api/v2/product';
+    let recordUrl = 'https://store-management-app.herokuapp.com/api/v2/sales';
     
     fetch(recordUrl, {
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
-            Authorization: `Bearer ${token}`,
-            'Content-type':'application/json'
+                    'Content-type':'application/json',
+                    "Authorization": "Bearer "+token
         },
-        body:JSON.stringify({id:product_id,price:price,quantity_to_sell:quantity,creator_name:creator})
+        body:JSON.stringify({product_id,price,quantity_to_sell})
         })
         .then((response) => response.json())
         .then((data) => {
